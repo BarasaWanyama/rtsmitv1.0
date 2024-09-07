3c076774610adac8e303a01049f7585f65cb73d8
Julius <Julius.wanyama2@gmail.com>
2024-09-07 14:18:19 +0300

    configuration updates to support server.test.js test success

--- jest.config.server.js
+++ jest.config.server.js
@@ -2,7 +2,9 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/src/**/*.test.js'],
   moduleFileExtensions: ['js', 'json', 'node'],
-  transform: {},
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest'
+  },
   testPathIgnorePatterns: ['/node_modules/', '/build/'],
   setupFilesAfterEnv: ['./jest.setup.server.js']
 };
